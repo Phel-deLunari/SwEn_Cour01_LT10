024dd71740952f387589a3408d3e05b5842d2f88
Nguyễn Minh Đức <23010171@st.phenikaa-uni.edu.vn>
2025-05-12 12:00:25 +0700

    Add files via upload
    Testing header

---
+++ Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Spotify Clone</h1>
+      <input type="text" placeholder="Search..." />
+    </div>
+  );
+};
+
+export default Header;
