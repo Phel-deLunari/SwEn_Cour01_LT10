5417afd5179ae908fb22afd02131dcb1e7e5800d
Nguyễn Minh Đức <23010171@st.phenikaa-uni.edu.vn>
2025-06-02 11:57:23 +0700

    Add files via upload

---
+++ Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <ul>
+        <li>Home</li>
+        <li>Your Library</li>
+        <li>Search</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
