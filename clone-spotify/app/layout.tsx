bff7a500701d09dd2c2d328d2b35ba4d10088fef
QlapMeowz1 <23010416@st.phenikaa-uni.edu.vn>
2025-06-03 20:35:23 +0700

    update

---
+++ clone-spotify/app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+import "./globals.css";
+import Sidebar from '@/components/Sidebar'
+import SupabaseProvider from '@/providers/SupabaseProvider'
+import UserProvider from '@/providers/UserProvider'
+import ModalProvider from "@/providers/ModalProvider";
+
+const FigtreeSans = Figtree({
+  variable: "--font-Figtree-sans",
+  subsets: ["latin"],
+});
+
+
+
+export const metadata: Metadata = {
+  title: "spotify",
+  description: "listen to your favorite music",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${FigtreeSans.variable} antialiased`}>
+      <SupabaseProvider>
+        <UserProvider>
+          <ModalProvider />
+        <Sidebar>
+        {children}
+      </Sidebar>
+      </UserProvider>
+      </SupabaseProvider>
+      </body>
+    </html>
+  );
+}
