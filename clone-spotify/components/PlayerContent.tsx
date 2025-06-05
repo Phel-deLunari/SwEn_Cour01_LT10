6185a5250fc2e4649804832f40937e4d4e13980b
Daishogunz <23010120@st.phenikaa-uni.edu.vn>
2025-06-06 04:01:59 +0700

    Update

--- clone-spotify/components/PlayerContent.tsx
+++ clone-spotify/components/PlayerContent.tsx
@@ -3,6 +3,10 @@
 import { Song } from "@/types";
 import MediaItem from "./MediaItem";
 import LikeButton from "./LikeButton";
+import {BsPauseFill, BsPlayFill} from 'react-icons/bs'
+import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
+import {HiSpeakerWave, HiSpeakerXMark} from "react-icons/hi2"
+import Slider from "./Slider";
 
  interface PlayerContentProps{
     song: Song
@@ -13,8 +17,10 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
     song,
     songUrl
 }) => {
+    const Icon = true ? BsPauseFill : BsPlayFill
+    const VolumeIcon = true ? HiSpeakerXMark : HiSpeakerWave
     return (
-        <div className="grid grid-cols md:grid-cols-3 h-full">
+        <div className="grid grid-cols-2 md:grid-cols-3 h-full">
             <div
                 className="
                 flex
@@ -26,6 +32,91 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
                         <LikeButton songId={song.id}/>
                     </div>
             </div>
+
+            <div className="
+                flex
+                md:hidden
+                col-auto
+                w-full
+                justify-end
+                items-center"
+            >
+                <div
+                    onClick={() => {}}
+                    className="
+                    h-10
+                    w-10
+                    flex
+                    items-center
+                    justify-center
+                    rounded-full
+                    bg-white
+                    p-1
+                    cursor-pointer
+                    "
+                >
+                    <Icon size={30} className="text-black"/>
+
+                </div>
+            </div>
+
+            <div className="
+                    hidden
+                    h-full
+                    md:flex
+                    justify-center
+                    w-full
+                    max-w-[722px]
+                    gap-x-6
+                "
+            >
+                <AiFillStepBackward
+                    onClick={()=>{}}
+                    size={30}
+                    className="
+                    text-neural-400
+                    cursor-pointer
+                    hover:text-white
+                    transition
+                    "
+                />
+                <div
+                    onClick={()=>{}}
+                    className="
+                        flex
+                        items-center
+                        justify-center
+                        h-10
+                        w-10
+                        rounded-full
+                        bg-white
+                        p-1
+                        cursor-pointer"
+                >
+                    <Icon size={30} className="text-black"/>
+                </div>
+                <AiFillStepForward 
+                    onClick={()=>{}}
+                    size={30}
+                    className="
+                    text-neural-400
+                    cursor-pointer
+                    hover:text-white
+                    transition"
+                />
+            </div>
+
+            <div className="hidden md:flex w-full justify-end pr-2">
+                <div className="flex items_center gap-x-2-w-[120px]">
+                    <VolumeIcon
+                        onClick={()=>{}}
+                        className="cursor-pointer"
+                        size={34}
+                    />
+                    <Slider/>
+                </div>
+            </div>
+
         </div>
 
     );
