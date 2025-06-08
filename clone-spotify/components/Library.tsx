6594d66d748cda8d51700909f5022c799c5d5add
Nguyễn Minh Đức <23010171@st.phenikaa-uni.edu.vn>
2025-06-08 23:28:31 +0000

    Update Library

--- clone-spotify/components/Library.tsx
+++ clone-spotify/components/Library.tsx
@@ -6,6 +6,7 @@ import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
 import { Song } from "@/types";
 import MediaItem from "./MediaItem";
+import useOnPlay from "@/hooks/useOnPlay";
 
 interface LibraryProps {
     songs: Song[];
@@ -17,6 +18,9 @@ const Library: React.FC<LibraryProps>= ({
     const authModal = useAuthModal();
     const upLoadModal = useUploadModal();
     const {user}= useUser();
+
+    const onPlay = useOnPlay(songs);
+
     const oneClick =() => {
         if (!user) {
             return authModal.onOpen();
@@ -73,7 +77,7 @@ const Library: React.FC<LibraryProps>= ({
             ">
              {songs.map((item) =>(
                 <MediaItem
-                    onClick={() => {}}
+                    onClick={(id: string) => onPlay(id)}
                     key={item.id}
                     data={item}
                 />
